9ae15bd8e5c5b73531758a16e8b9ca399a9d35bc
bconfiden2 <bconfiden2@naver.com>
2022-06-19 21:58:30 +0900

    프로세스 자동 재시작

--- server/process-alive.sh
+++ server/process-alive.sh
@@ -2,6 +2,8 @@
 
 # 모니터링할 프로세스 지정
 process_name="/usr/bin/kubelet"
+# 해당 프로세스 실행시킬 수 있는 명령어
+process_start="systemctl restart kubelet"
 # 최대 실행시킬 프로세스 수
 threshold=3
 
@@ -12,6 +14,8 @@ cnt=$(ps ax -o command | grep $process_name | grep -v "^grep" | wc -l)
 # 해당 결과가 0이면 프로세스가 존재하지 않는다는 뜻
 if [ $cnt -eq 0 ]; then
     echo $process_name not exist! >&2
+    # 프로세스 자동 재시작
+    $process_start
 fi
 
 if [ $cnt -gt $threshold ]; then
